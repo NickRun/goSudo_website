69c88599f83a399422a6f3a9452433c3b39f0a29
nscipione <nscipione@gmail.com>
2014-10-14 19:05:27 -0400

    mobile toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,22 @@ jQuery(function($) {
 	  	//enabling stickUp on the '.navbar-wrapper' class
 	  	$('#site-nav').stickUp();
 	  	//toggle mobile menu from menu icon
-	    $(".menu-icon").click(toggleMenu);
+	    $(".menu-icon").on('click', function(){
+		    toggleMenu();
+		});
 	    //close mobile menu from clicking a link within nav
-	    $(".site-nav-links a").click(toggleMenu);
+	    $(".site-nav-links a").on('click', function(){
+		    toggleMenu();
+		});
 	});
 });
 
+
 function toggleMenu() {
-    $(".site-nav-links").slideToggle();
+   	if ( $(".site-nav-links").hasClass("activate") ) {
+		$('.site-nav-links').removeClass('activate');
+	}
+	else {
+		$('.site-nav-links').addClass('activate');
+	};
 }
